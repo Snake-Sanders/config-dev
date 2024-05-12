3f4072f7b136bf239ead205f4da38ec795025fa1
Snake Sanders <elmarian22@gmail.com>
2024-05-12 22:12:46 +0200

    fixed typo in catppuccin config

--- .config/nvim/lua/plugins/catppuccin.lua
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -3,7 +3,23 @@ return {
   "catppuccin/nvim",
   name = "catppuccin",
   priority = 1000,
-  confing = function()
+  config = function()
+   local catppuccin = require("catppuccin")
+   
+   catppuccin.setup({
+      flavour = "macchiato",
+      integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        treesitter = true,
+        notify = false,
+        mini = {
+          enabled = true,
+          indentscope_color = "",
+        },
+      }
+    })
     vim.cmd.colorscheme("catppuccin")
   end,
 }
