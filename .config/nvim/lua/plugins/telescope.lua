2e926d2696f3420e71d8840e3e058cb79cfa71aa
Snake Sanders <elmarian22@gmail.com>
2024-06-08 13:56:03 +0200

    add telescope keymap

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,46 +1,36 @@
 -- telescope for fuzzy find file
 return {
-  {
-  'nvim-telescope/telescope.nvim', tag = '0.1.6',
-  dependencies = { 'nvim-lua/plenary.nvim' },
-  config = function()
-    local builtin = require("telescope.builtin")
-    -- ctr + p = search files
-    vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-    -- space + f + g = find in files with grep 
-    vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-  end
-  },
-  {
-    -- to show code actions (ca) in telescope
-    "nvim-telescope/telescope-ui-select.nvim",
-    config = function()
-      require("telescope").setup ({
-        extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown {
-            }
-          }
-        }
-      })
-      require("telescope").load_extension("ui-select")
-    end
-  }
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.6",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			local builtin = require("telescope.builtin")
+			-- ctr + p = search files in git repo only
+			vim.keymap.set("n", "<C-p>", builtin.git_files, {})
+			-- space + p + f = search files
+			vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
+      -- space + p + s = find in files with grep
+      -- not this keymap requires ripgrep to be installed
+      vim.keymap.set("n", "<leader>ps", function()
+        builtin.grep_string({search = vim.fn.input("Grep > ")})
+      end)
+		end,
+	},
+	{
+		-- to show code actions (ca) in telescope
+		"nvim-telescope/telescope-ui-select.nvim",
+		config = function()
+			require("telescope").setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
+			})
+			require("telescope").load_extension("ui-select")
+		end,
+	},
 }
 
-    -- key remaps
-    -- ctr + p = search files
-    -- vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-    -- space + f + g = find in files with grep 
-    --vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-    -- space + p + v = previous view
-    --vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
-    -- space + p + f = path find (all the files)
-    --vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-    -- space + p + g = path find only files in git repo
-    --vim.keymap.set('n', '<leader>pg', builtin.git_files, {})
--- space + p + s = project search
---vim.keymap.set('n', '<leader>ps', function()
---  builtin.grep_string({search = vim.fn.input("Grep >")})
---end)
---
+
