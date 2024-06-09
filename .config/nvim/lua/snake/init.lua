6ce4e1764858e9ba38943a87e452201d59f8bb52
Snake Sanders <elmarian22@gmail.com>
2024-06-09 10:45:08 +0200

    highlight when yanking

--- .config/nvim/lua/snake/init.lua
+++ .config/nvim/lua/snake/init.lua
@@ -30,7 +30,19 @@ vim.opt.updatetime = 50
 
 -- vertical column (rule) delimiter
 vim.opt.colorcolumn = "80"
---
+
+-- [[ Basic Autocommands ]]
+--  See `:help lua-guide-autocommands`
+
+-- Highlight when yanking (copying) text
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
 -- key remap
 require("snake.remap")
 
