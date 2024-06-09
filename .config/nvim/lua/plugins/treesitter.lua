03beee13b75f6b9d790b47b53f3a76430cf401bb
Snake Sanders <elmarian22@gmail.com>
2024-06-09 12:56:47 +0200

    add extra lang

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -4,8 +4,8 @@ return {
   config = function()
     local configs = require("nvim-treesitter.configs")
     configs.setup({
-      ensure_installed = { "c", "lua", "elixir", "eex", "heex", "javascript", "html", "rust" },
-      highlight = { enable = true },
+      ensure_installed = { 'bash', 'c', 'diff', 'elixir', 'eex', 'heex',  'javascript', 'html', 'lua', 'luadoc', 'markdown', 'rust', 'vim', 'vimdoc' },
+      highlight = { enable = true, disable = { 'ruby' } },
       indent = { enable = true },
     })
   end,
